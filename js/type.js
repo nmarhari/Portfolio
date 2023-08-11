1126b162c23d796e9c944316ce7f034d76d9147e
nmarhari <nassimmarhari@gmail.com>
2023-08-11 13:54:15 -0400

    backDelay

--- js/type.js
+++ js/type.js
@@ -1,7 +1,8 @@
 var typed = new Typed('.dynamic-text', {
-    strings: ['code together^2000', 'create together^2000', 'innovate together^2000'],
+    strings: ['code together', 'create together', 'innovate together'],
     typeSpeed: 50,
     backSpeed: 50,
+    backDelay: 2500,
     loop: true,
     smartBackspace: false,
 });
