862e0c96c36763f8b0b6926ab44a955000e394f1
nmarhari <nassimmarhari@gmail.com>
2023-08-11 13:49:45 -0400

    spacing

--- js/type.js
+++ js/type.js
@@ -1,6 +1,7 @@
 var typed = new Typed('.dynamic-text', {
-    strings: ['code together', 'create together', 'innovate together'],
-    typeSpeed: 120,
-    backSpeed: 120,
+    strings: ['code together^2000', 'create together^2000', 'innovate together^2000'],
+    typeSpeed: 50,
+    backSpeed: 50,
     loop: true,
+    smartBackspace: false,
 });
