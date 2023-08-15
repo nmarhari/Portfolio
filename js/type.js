e25458955eddc6ce2b904ef5605b006c66a167b8
Nassim Marhari <nassimmarhari@gmail.com>
2023-08-15 15:23:38 -0400

    added build

--- js/type.js
+++ js/type.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.dynamic-text', {
-    strings: ['code together', 'create together', 'innovate together'],
+    strings: ['code together', 'create together', 'innovate together', 'build together'],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 2500,
