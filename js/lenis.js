a4fee71f7cd5a46a43ff19a3f7c0ba6fe29113f4
nmarhari <nassimmarhari@gmail.com>
2023-08-16 22:23:57 -0400

    fill in info

--- js/lenis.js
+++ js/lenis.js
@@ -15,7 +15,7 @@ const section_1 = document.getElementById("about");
 const col_left = document.querySelector(".col_left");
 const timeln = gsap.timeline({ paused: true });
 
-timeln.fromTo(col_left, {y: 0}, {y: '100vh', duration: 1, ease: 'none'}, 0);
+timeln.fromTo(col_left, {y: 0}, {y: '110vh', duration: 1, ease: 'none'}, 0);
 
 const scroll_1 = ScrollTrigger.create({
     animation: timeln,
